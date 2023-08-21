c42b18821fe6226173365bb9dbe6c4016128bd7c
h-nimrod <h.tanabe@gmail.com>
2023-08-21 23:02:34 +0900

    fix

--- send_test.py
+++ send_test.py
@@ -6,8 +6,7 @@ SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
 
 def get_access_token(private_key_json):
   """Retrieve a valid access token that can be used to authorize requests.                                                                                        
-
-  :return: Access token.                                                                                                                                            """
+  :return: Access token."""
 
   credentials = ServiceAccountCredentials.from_json_keyfile_name(private_key_json, SCOPES)
   access_token_info = credentials.get_access_token()
@@ -23,4 +22,4 @@ if __name__ == '__main__':
     private_key_json = args.private_key_json_file
     
 
-  print(get_access_token(args.private_key_json_file))
+  print(get_access_token(private_key_json))
