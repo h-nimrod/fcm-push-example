49ac54ff95356dc5e975a2bab9fc67a14a37366b
h-nimrod <h.tanabe@gmail.com>
2023-08-23 03:03:56 +0900

    add get_message_json()

--- send_test.py
+++ send_test.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 PRIVATE_KEY_JSON = 'sample-app-firebase-adminsdk-foobar.json' ### modify to match your environment
@@ -17,6 +18,26 @@ def get_access_token(private_key_json):
   credentials = ServiceAccountCredentials.from_json_keyfile_name(private_key_json, SCOPES)
   access_token_info = credentials.get_access_token()
   return access_token_info.access_token
+
+def get_message_json(target_device_token, template_json_file):
+  """
+  Load a message template from a JSON file and set the target device token.
+
+  Parameters:
+  - target_device_token (str): The token of the target device.
+  - template_json_file (str, optional): Path to the JSON file that contains the message template. Defaults to 'sample_message.json'.
+
+  Returns:
+  - dict: A dictionary representing the loaded message template, with the token set to the provided target device token.
+  """
+
+  with open(template_json_file, 'r', encoding='utf-8') as file:
+    obj = json.load(file)
+
+  if target_device_token != None:
+    obj["message"]["token"] = target_device_token
+
+  return obj
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
