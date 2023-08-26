ed2b7fa30e1da33c9082dea366a048c591ff5daa
h-nimrod <h.tanabe@gmail.com>
2023-08-26 15:54:51 +0900

    changed the indentation to 4 spaces.

--- send_test.py
+++ send_test.py
@@ -3,125 +3,116 @@ import json
 import requests
 from oauth2client.service_account import ServiceAccountCredentials
 
-PRIVATE_KEY_JSON = 'sample-app-firebase-adminsdk-foobar.json' ### modify to match your environment
+PRIVATE_KEY_JSON = 'sample-app-firebase-adminsdk-foobar.json'  # modify to match your environment
 DEFAULT_MESSAGE_JSON = 'sample_message.json'
 SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
 
+
 def get_access_token(private_key_json):
-  """
-  Retrieve a valid access token that can be used to authorize requests.
+    """
+    Retrieve a valid access token that can be used to authorize requests.
 
-  Parameter:
-  - private_key_json (str): Path to the JSON file containing the service account's private key.
+    Parameter:
+    - private_key_json (str): Path to the JSON file containing the service account's private key.
 
-  Returns: Access token (str).
-  """
+    Returns: Access token (str).
+    """
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(private_key_json, SCOPES)
+    access_token_info = credentials.get_access_token()
+    return access_token_info.access_token
 
-  credentials = ServiceAccountCredentials.from_json_keyfile_name(private_key_json, SCOPES)
-  access_token_info = credentials.get_access_token()
-  return access_token_info.access_token
 
 def get_message_json(target_device_token, template_json_file):
-  """
-  Load a message template from a JSON file and set the target device token.
+    """
+    Load a message template from a JSON file and set the target device token.
 
-  Parameters:
-  - target_device_token (str): The token of the target device.
-  - template_json_file (str, optional): Path to the JSON file that contains the message template. Defaults to 'sample_message.json'.
+    Parameters:
+    - target_device_token (str): The token of the target device.
+    - template_json_file (str, optional): Path to the JSON file that contains the message template.
+      Defaults to 'sample_message.json'.
 
-  Returns:
-  - dict: A dictionary representing the loaded message template, with the token set to the provided target device token.
-  """
+    Returns:
+    - dict: A dictionary representing the loaded message template, with the token set to the provided target device token.
+    """
+    with open(template_json_file, 'r', encoding='utf-8') as file:
+        obj = json.load(file)
 
-  with open(template_json_file, 'r', encoding='utf-8') as file:
-    obj = json.load(file)
+    if target_device_token:
+        obj["message"]["token"] = target_device_token
 
-  if target_device_token != None:
-    obj["message"]["token"] = target_device_token
+    return obj
 
-  return obj
 
 def get_project_id_from_json(private_key_json_file):
-  """
-  Retrieve the project ID from a given private key JSON file.
+    """
+    Retrieve the project ID from a given private key JSON file.
 
-  Parameters:
-  - private_key_json (str): Path to the JSON file that contains the private key information.
+    Parameters:
+    - private_key_json (str): Path to the JSON file that contains the private key information.
 
-  Returns:
-  - str: The project ID extracted from the JSON file.
-  """
+    Returns:
+    - str: The project ID extracted from the JSON file.
+    """
+    with open(private_key_json_file, 'r', encoding='utf-8') as file:
+        obj = json.load(file)
 
-  with open(private_key_json_file, 'r', encoding='utf-8') as file:
-    obj = json.load(file)
+    return obj["project_id"]
 
-  return obj["project_id"]
 
 def send_push_message(access_token, device_token, private_key_json_file=PRIVATE_KEY_JSON, message_json_file=DEFAULT_MESSAGE_JSON):
-  """
-  Send a push notification via Firebase Cloud Messaging (FCM).
+    """
+    Send a push notification via Firebase Cloud Messaging (FCM).
 
-  This method sends a push notification to a specific device using FCM.
-  The content of the notification is based on the provided JSON template file.
-  If no template is provided, a default one is used. The project ID necessary for the FCM
-  request is derived from a provided private key JSON file, or a default if none is given.
+    This method sends a push notification to a specific device using FCM.
+    The content of the notification is based on the provided JSON template file.
+    If no template is provided, a default one is used. The project ID necessary for the FCM
+    request is derived from a provided private key JSON file, or a default if none is given.
 
-  Parameters:
-  - access_token (str): The access token used for authentication with the FCM service.
-  - device_token (str): The unique token of the target device to which the notification should be sent.
-  - private_key_json_file (str, optional): The path to a JSON file containing the private key
-    information used to derive the project ID. Defaults to PRIVATE_KEY_JSON.
-  - message_json_file (str, optional): The path to a JSON file containing the message template
-    for the notification. Defaults to DEFAULT_MESSAGE_JSON.
-  """
+    Parameters:
+    - access_token (str): The access token used for authentication with the FCM service.
+    - device_token (str): The unique token of the target device to which the notification should be sent.
+    - private_key_json_file (str, optional): The path to a JSON file containing the private key
+      information used to derive the project ID. Defaults to PRIVATE_KEY_JSON.
+    - message_json_file (str, optional): The path to a JSON file containing the message template
+      for the notification. Defaults to DEFAULT_MESSAGE_JSON.
+    """
+    project_id = get_project_id_from_json(private_key_json_file)
+    url = f'https://fcm.googleapis.com/v1/projects/{project_id}/messages:send'
 
-  project_id = get_project_id_from_json(private_key_json_file)
-  url = f'https://fcm.googleapis.com/v1/projects/{project_id}/messages:send'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json; UTF-8',
+    }
 
-  headers = {
-    'Authorization': f'Bearer {access_token}',
-    'Content-Type': 'application/json; UTF-8',
-  }
+    message = get_message_json(target_device_token=device_token, template_json_file=message_json_file)
 
-  message = get_message_json(target_device_token=device_token, template_json_file=message_json_file)
+    print("=== Request ===")
+    print("Request message: ", json.dumps(message, indent=4, ensure_ascii=False))
 
-  print("=== Request ===")
-  # print("Request headers: ", json.dumps(headers, indent=4, ensure_ascii=False))
-  print("Request message: ", json.dumps(message, indent=4, ensure_ascii=False))
+    response = requests.post(url, headers=headers, data=json.dumps(message))
 
-  response = requests.post(url, headers=headers, data=json.dumps(message))
-
-  print("")
-  print("=== Response ===")
-  print("Response status code: ", response.status_code)
-  print("Response content: ", response.text)
+    print("\n=== Response ===")
+    print("Response status code: ", response.status_code)
+    print("Response content: ", response.text)
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-d', '--device-token', help="give taget device token")
-  parser.add_argument('-p', '--private-key-json-file', help="give private key json file")
-  parser.add_argument('-m', '--message-json', help="give message json")
-  parser.add_argument('--print-access-token', action='store_true', help="print access token")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--device-token', help="give taget device token")
+    parser.add_argument('-p', '--private-key-json-file', help="give private key json file")
+    parser.add_argument('-m', '--message-json', help="give message json")
+    parser.add_argument('--print-access-token', action='store_true', help="print access token")
+    args = parser.parse_args()
 
-  device_token = None
-  if args.device_token:
-    device_token = args.device_token
+    device_token = args.device_token if args.device_token else None
+    private_key_json = args.private_key_json_file if args.private_key_json_file else PRIVATE_KEY_JSON
+    message_json = args.message_json if args.message_json else DEFAULT_MESSAGE_JSON
 
-  private_key_json = PRIVATE_KEY_JSON
-  if args.private_key_json_file:
-    private_key_json = args.private_key_json_file
+    access_token = get_access_token(private_key_json)
 
-  message_json = DEFAULT_MESSAGE_JSON
-  if args.message_json:
-    message_json = args.message_json
+    if args.print_access_token:
+        print("=== access token ===")
+        print(access_token)
+        print("")
 
-  access_token = get_access_token(private_key_json)
-
-  if args.print_access_token:
-    print("=== access token ===")
-    print(access_token)
-    print("")
-
-  send_push_message(access_token=access_token, device_token=device_token, private_key_json_file=private_key_json, message_json_file=message_json)
+    send_push_message(access_token=access_token, device_token=device_token, private_key_json_file=private_key_json, message_json_file=message_json)
